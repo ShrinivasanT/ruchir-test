761bca0d138d168ea23bb65f9c69d960b681f32b
Shrinivasan Thevar <160501899+ShrinivasanT@users.noreply.github.com>
2026-01-11 13:55:48 +0530

    wd

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
+import pandas
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms, models
+import cv2
 import math
 import os
 import random
 import re
 import sys
 
+device = torch.device("cuda")
 def formingMagicSquare(s):
     magic_squares = [
         [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
